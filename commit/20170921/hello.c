d916583dec177b3016f2c8b65289bde6126411ac
luoxiaoyu <1192548434@qq.com>
2017-09-29 18:43:47 +0800

    Merge branch 'master' of github.com:NNU-CS16/TCPL17

---
+++ commit/20170921/hello.c
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+#include <stdio.h>
+int main()
+{
+ printf("Hello World.\n");
+ return 0;
+}
+
+=======
+/*This is the first C program.*/
+#include <stdio.h>
+int main ()
+{
+    printf("Hello World.");
+    return 0;
+}
+
+
+>>>>>>> 2882a092fe091c07dd73c001e30c7d4e75972184
