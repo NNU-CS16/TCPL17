1299025c6c0d0d4590dd1026147eaa6cecba04f3
Jiayu Luo <2392215456@qq.com>
2017-09-30 19:39:08 +0800

    Add work for 20179321 -19170321  Jiayu Luo

--- commit/19170321/hello.c
+++ commit/19170321/hello.c
@@ -1,7 +1,7 @@
-/*This is the first C program.*/
+
 #include <stdio.h>
 int main( )
 {
-    printf("Hello.\n");
+    printf("Hello.\n");     
     return 0;
 }
--- commit/20170921/19170321/hello.c
+++ commit/20170921/19170321/hello.c
@@ -1,4 +1,4 @@
-/*This is the first C program*/
+
 #include <stdio.h>
 int main( )
 {
