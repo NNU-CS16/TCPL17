a294fd12bb682439718e46b39b5f25861255af54
juntao <1968768442@qq.com>
2017-10-03 14:20:08 +0800

    Add work for 20170930-19170323 juntao qu

---
+++ commit/20170930/19170323/P17_2_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  signed long int a;
+  unsigned long int b;
+  signed short int c;
+  unsigned short int d;
+
+
+   printf("sjgned long int  size=%ld\n",sizeof(a));
+   printf("unsigned long int  size=%ld\n",sizeof(b));
+   printf("signed short int  size=%ld\n",sizeof(c));
+   printf("unsigned short  int  size=%ld\n",sizeof(d));
+
+   return 0;
+}
---
+++ commit/20170930/19170323/parity.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n=10;
+  int flag;
+  if(n%2==0)
+  {flag=0;
+   printf("%d\n",flag);
+  }
+  else
+  {flag=1;
+  printf("%d\n",flag);
+  }
+  
+   return 0;
+}
