6ba2b7de43a8fd8590b6294e472e876fc62ee9a0
“Xinyi <gli_glu@163.com>
2017-10-04 21:00:33 -0700

    Add work for 20170930-19170307,Xinyi Xu

---
+++ commit/20170930/19170307/P17_2_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    int i;
+    float x;
+    double z;
+
+    printf("char size=%d\n",sizeof(c));
+    printf("int size=%d\n",sizeof(i));
+    printf("float size=%d\n",sizeof(x));
+    printf("double size=%d\n",sizeof(z));
+
+    return 0;
+}
