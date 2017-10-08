b7e1c94444ebf6724a44cdc2e580bb46777df9d7
“Yuhan <1013829049@qq.com>
2017-10-08 17:56:06 +0800

    Add work for 170930-19170301-Yuhan Wang

---
+++ commit/20170930/19170301/P17_2_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	int b;
+	float c;
+	double d;
+
+	 printf("char  size=%ld\n",sizeof(a));
+	 printf("int   size=%ld\n",sizeof(b));
+	 printf("float size=%ld\n",sizeof(c));
+	 printf("double size=%ld\n",sizeof(d));
+	  
+	 return 0;
+}
+
