8e5473db533ab64b5049c5a7eaeb98dafd375deb
“19170322” <2146638607@qq.com>
2017-10-10 20:11:21 +0800

    add work for 20170930 -- 19170322 zhengyi

--- commit/20170930/19170322/P31_1_3.c
+++ commit/20170930/19170322/P31_1_3.c
@@ -2,6 +2,7 @@
 int main()
 {
   char a='B',b=33;
+  char c='0',d='9';
   a=a-'A'+'0';
   b=b*2;
   printf("%c,%c\n",a,b);
---
+++ commit/20170930/19170322/tcpl_2_7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int invert(int x,unsigned int p,unsigned int n)
+{
+	int a=0;
+	int i;
+	for(i=0;i<n;i++)
+	a|=1<<i;
+	a=a<<(p+1-n);
+	return x^a;
+}
+ int main()
+{
+	int i;
+	i=invert(42,23,6);
+	printf("%d\n",i);
+	return 0;
+}
