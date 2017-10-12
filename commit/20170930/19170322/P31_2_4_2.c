37b40d1e9db8188857ee7bd6dec1aa9b0cbb7948
“19170322” <2146638607@qq.com>
2017-10-12 10:38:46 +0800

    a work form zhengyiX

--- commit/20170930/19170322/P31_2_4_2.c
+++ commit/20170930/19170322/P31_2_4_2.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-
 int main()
 {
-   float x,y;
-   scanf("%e",&x);
-   y=x*(x*x)+2*x*x+3*x+1;
-   printf("%f\n",y);
-			 
-			    return 0; 
-
+    	
+    float x,a,b,c;
+    scanf("%e,%e,%e,%e",&x,&a,&b,&c);
+    int d = fabs(x);
+    int e = sqrt(d);
+    if (e=(4*a)/(b*c))
+    printf("不相等");
+    else printf("相等");
+			  
+    return 0;
 }
