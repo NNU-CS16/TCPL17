163f6868cf86fa4e727c88f7c15df9399e72ffe7
root <2365694766@qq.com>
2017-10-13 14:35:37 +0800

    add work...

--- commit/20170930/22171913/P17_2_1.c
+++ commit/20170930/22171913/P17_2_1.c
@@ -5,19 +5,19 @@ int main()
     int i;
     float x;
     double z;
-    signed long int v; 
-    unsigned int b;
-    short int m;
-    unsigned short int y; 
- 
+    long int h;
+    unsigned int m;
+    short int n;
+    unsigned short int y;
+
     printf("char size=%d\n",sizeof(c));
     printf("int size=%d\n",sizeof(i));
     printf("float size=%d\n",sizeof(x));
     printf("double size=%d\n",sizeof(z));
-    printf("signed long int size=%d\n",sizeof(v));
-    printf("unsigned int size=%d\n",sizeof(b));
-    printf("short int size=%d\n",sizeof(m));
+    printf("long int size=%d\n",sizeof(h));
+    printf("unsigned int size=%d\n",sizeof(m));
+    printf("short int size=%d\n",sizeof(n));
     printf("unsigned short int size=%d\n",sizeof(y));
- 
+
     return 0;
-}
+} 
