6c937cda070b19212bdc4a07cab88d75ec9b70e0
root <422329680@qq.com>
2017-10-15 01:50:02 +0800

    Add work for 20171012-22171902,Hanyu Fang

---
+++ commit/20171012/22171902/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+     long   int sum,n;
+        sum=0;
+        for(n=2;n<=100;n+=2)
+    {  
+	sum+=n;
+    }      
+  printf("the value of '2+4+6+...+100'is %d\n",sum);
+        return 0;
+}
+
+
+
