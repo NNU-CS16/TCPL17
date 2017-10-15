def1a370a9597cbbf2cc2f7212757376a5416000
huimin yu <635824889@qq.com>
2017-10-15 23:28:50 +0800

    Add work for 20170930-19170332,Huimin Yu

---
+++ commit/20170930/19170332/P31_2_4_2.c
@@ -0,0 +1,17 @@
+    #include<stdio.h>
+    #include<math.h>
+
+    int main()
+
+    {
+    
+        float x,a,b,c;
+
+        scanf("%f,%f,%f,%f\n",&x,&a,&b,&c);
+
+        printf("sqrt(fabs(x))!=(4*a)/(b*c)");
+ 
+        return 0;
+
+    }
+
