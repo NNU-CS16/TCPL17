2ae71f0b23ae8a5484636fdc775a7613dbdb2433
李锦 <442910230@qq.com>
2017-10-15 04:31:42 -0700

    Add work for 19170310--Jin Li

---
+++ commit/20170930/19170310/p31_1_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+  char a='B',b=33;
+  char c='0',d='9';
+  a=a-'A'+'0';
+b=b*2;
+printf("%c,%c\n",a,b);
+c++,d--;
+printf("%c,%c\n",a,b);
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
