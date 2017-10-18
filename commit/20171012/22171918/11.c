77bdb9cec39230c1429b2db56aa488b3844480db
Zhou Yang <1802870617@qq.com>
2017-10-18 19:15:53 +0800

    Add work for 20171012 - 22171918,Zhou Yang

--- commit/20171012/22171918/11.c
+++ commit/20171012/22171918/11.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 int main ()
 {
-  int start;
-  scanf("%d",&start);
-  int end;
-  end=start+4;
-  int a,b,d;
-  for(a=start;a<end;a++)
-  {
-    for(b=start;b<end;b++)
-    {
-      for(d=start;d<end;d++)
-      {
-        if(a==b&&b==d&&a==d)
-        {
-          printf("%d",a*100+b*10+d);
-        }
-        else
-        {
-          printf("%d\n",a*100+b*10+d);
-        }
-      }
-    }
-  }
- return 0;
+   int a,b,c;
+   int x,y;
+   y=1;
+   scanf("%d",&x);
+   for (a=x;a<=x+3;a++)
+    for (b=x;b<=x+3;b++)
+      for (c=x;c<=x+3;c++)
+        if (a!=b && b!=c && c!=a) 
+          if (y<6) 
+           {
+            y++;
+            printf("%d%d%d ",a,b,c); 
+           }
+          else
+          {
+           y=1;
+           printf("%d%d%d\n",a,b,c);
+          }
+    return 0;
 }
