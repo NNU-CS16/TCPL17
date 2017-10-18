103f8ca94ae55f58028d76ec6a0a5dcfece80be2
22171903 <1814121481@qq.com>
2017-10-18 20:18:53 +0800

    This is the third C programe homework.

---
+++ commit/20171012/22171903/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n,a,x;
+    printf("a=");
+    scanf("%d",&a);
+    printf("n=");
+    scanf("%d",&n);
+
+    for(a,x=0;n>=1;n--)
+        x=x+pow(a,n);
+    printf("a+aa+...+aaa...a(n个a)=%d\n",x);
+}
