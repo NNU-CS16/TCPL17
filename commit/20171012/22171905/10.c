7a10b89b853a2f989e1e045bbb3903ea2f3b55fa
qianqian ran <2369677542@qq.com>
2017-10-19 21:59:10 +0800

    Add work for 20171012 - 22171905qianqianran

---
+++ commit/20171012/22171905/10.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+double f(double,double,double,double,double);
+int main()
+{
+double a3,a2,a1,a0;
+scanf("%1f %1f %1f %1f",&a3,&a2,&a1,&a0);
+double a,b,mid,out,oa;
+scanf("%1f %1f",&a,&b);
+if(a>b) {double temp;temp=a;a=b;b=temp;}
+if(b-a<0.001) {pintf("%1f",(a+b/2);return 0;}
+    while(1)
+{
+    mid=(a+b)/2;
+    out=f(mid,a3,a2,a1,a0);
+    oa=f(a,a3,a2,a1,a0);
+if(fabs(out)<0.001) {printf("the out put is %.2f\n",mid);break
+else {if(out*oa>0) a=mid;
+else b=mid;}
+    return 0;
+}#include <stdio.h>
+#include <math.h>
+double f(double,double,double,double,double);
+int main()
+{
+double a3,a2,a1,a0;
+scanf("%1f %1f %1f %1f",&a3,&a2,&a1,&a0);
+double a,b,mid,out,oa;
+scanf("%1f %1f",&a,&b);
+if(a>b) {double temp;temp=a;a=b;b=temp;}
+if(b-a<0.001) {pintf("%1f",(a+b/2);return 0;}
+    while(1)
+{
+    mid=(a+b)/2;
+    out=f(mid,a3,a2,a1,a0);
+    oa=f(a,a3,a2,a1,a0);
+if(fabs(out)<0.001) {printf("the out put is %.2f\n",mid);break
+else {if(out*oa>0) a=mid;
+else b=mid;}
+    return 0;
+}#include <stdio.h>
+#include <math.h>
+double f(double,double,double,double,double);
+int main()
+{
+dcouble a3,a2,a1,a0;
+scanf("%1f %1f %1f %1f",&a3,&a2,&a1,&a0);
+double a,b,mid,out,oa;
+scanf("%1f %1f",&a,&b);
+if(a>b) {double temp;temp=a;a=b;b=temp;}
+if(b-a<0.001) {pintf("%1f",(a+b/2);return 0;}
+    while(1)
+{
+    mid=(a+b)/2;
+    out=f(mid,a3,a2,a1,a0);
+    oa=f(a,a3,a2,a1,a0);
+if(fabs(out)<0.001) {printf("the out put is %.2f\n",mid);break
+else {if(out*oa>0) a=mid;
+else b=mid;}
+}
+    return 0;
+}
+
+double f(double n,double a3,double a2,double a1,double a0)
+{
+return a3*n*n*n+a2*n*n+a1*n+a0;
+}
