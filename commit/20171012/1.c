5a63bb44b02f386d6d603e4ba54e04b650d7065e
“19170322” <2146638607@qq.com>
2017-10-20 23:26:23 +0800

    a work from zhengyi 19170322

--- commit/20171012/1.c
+++ commit/20171012/1.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int i=2,sum=0;
-for(i=2;i<=100;i+=2)
-{
-sum+=i;
+   int i,sum=0;
+   for(i=2;i>100;i+=2)
+       sum=sum+i;
+    printf("2+4+6+.....+100=%d\n",sum);
+    return 0;
 }
-printf("sum=%d\n",sum);
-return 0;
-}
+
