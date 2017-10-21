64a9330c4d1facb566bdb0402aa1801a6a59d60e
Zhuojun Zhong <742889866@qq.com>
2017-10-21 22:27:30 +0800

    Add work for 22171012 – 22171920,Zhuojun Zhong

--- commit/20171012/22171920/11.c
+++ commit/20171012/22171920/11.c
@@ -1,15 +1,15 @@
   #include <stdio.h>  
     int main()  
     {  
-        int start;  
-        scanf("%d", &start);  
+        int m;  
+        scanf("%d", &m);  
         int end;  
         int n = 0;  
-        end = start + 4;  
-        for(int i = start; i<end; i++){  
-                for(int j = start; j<end; j++){  
+        end = m + 4;  
+        for(int i = m; i<end; i++){  
+                for(int j = m; j<end; j++){  
                         if(i == j) continue;  
-                        for(int k = start; k<end; k++){  
+                        for(int k = m; k<end; k++){  
                              if(k == j) continue;  
                          if(k == i) continue;  
                       n++;  
