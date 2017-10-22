bfc2a69584aacf26f848a41099ba2a2a2400d63b
李锦 <442910230@qq.com>
2017-10-22 07:50:01 -0700

    add work for 20171012 Jin Li 19170310

--- commit/20171012/19170310/5.c
+++ commit/20171012/19170310/5.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-#include<math.h>
+
 
 int main()
 {
  int x,y,z;
- for(x=1;x<=9;x++)
- {
-  for(y=1;y<=x;y++) 
+ for(x=1;x<=9;x=x+1){ 
+
+  for(y=1;y<=9;y=y+1) 
    {
    z=x*y;
-   printf("%d*%d=%d  ",y,x,z);
+   printf("%d*%d=%d\t",y,x,z);
    }
  printf("\n");
  }
@@ -21,4 +21,4 @@ int main()
 
 
 
-}
+
