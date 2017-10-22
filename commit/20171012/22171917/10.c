deccce1d3e89c13157f3c18915056e0216d54030
Luoxiaoyu <1192548434@qq.com>
2017-10-22 21:38:15 +0800

    Add work for 20171012 - 22171917,luoxiaoyu

---
+++ commit/20171012/22171917/10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int x,y,a3,a2,a1,a0,m,n,k,z,q,r,x1,x2,x3,a,b,j;
+    (a<=x)<=b;
+    y=a3*x*x*x+a2*x*x+a1*x+a0;
+    if((z=a3*a*a*a+a2*a*a+a1*a+a0)<(j=a3*b*b*b+a2*b*b+a1*b+a0))
+      m=(a+b)/2;
+      n=a3*m*m*m+a2*m*m+a1*m+a0;
+    if(n=0)
+      printf("x1=%d\n",x1=(a+b)/2);
+    while((n*z)>0)
+    {
+      a=(a+b)/2;
+      q=(a+b)/2;
+      n=a3*q*q*q+a2*q*q+a1*q+a0;
+    }
+     if(n=0)
+      printf("x2=%d\n",x2=(a+b)/2);
+    while(n*j<0)
+    {
+       b=(a+b)/2;
+       r=(a+b)/2;
+       n=a3*r*r*r+a2*r*r+a1*r+a0;
+    }
+       if(n=0)
+      printf("x3=%d\n",x3=(a+b)/2);
+    
+      return 0;
+}
+
