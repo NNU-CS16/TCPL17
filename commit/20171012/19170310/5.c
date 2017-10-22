15740e78661bc59bfe72775c9e547eec47a973b4
李锦 <442910230@qq.com>
2017-10-22 06:50:50 -0700

    add work for 20171012 Jin Li 19170310

---
+++ commit/20171012/19170310/5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+ int x,y,z;
+ for(x=1;x<=9;x++)
+ {
+  for(y=1;y<=x;y++) 
+   {
+   z=x*y;
+   printf("%d*%d=%d  ",y,x,z);
+   }
+ printf("\n");
+ }
+ return 0;
+}
+
+
+
+
+
+
+}
