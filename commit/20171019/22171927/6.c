2daa2eeedab74e936c05fa5d18153e9fd2ee2c9a
root <837491291@qq.com>
2017-10-26 22:11:22 +0800

    Add work for 20171019 22171927 BingQing Xu

---
+++ commit/20171019/22171927/6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+        int a,b,c;
+        scanf("%d %d %d",&a,&b,&c);
+        if(a!=b&&b==c){
+                printf("A");
+        } else if(b!=a&&a==c){
+                printf("B");
+        }else{
+                printf("C");
+        }
+        return 0;
+}
