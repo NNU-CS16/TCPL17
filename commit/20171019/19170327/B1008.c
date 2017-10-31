9978bbadd4eae25b89c1092192d7333bebac9fd6
19170327 <296951663@qq.com>
2017-10-31 23:42:02 +0800

    Add Work For 20171019 19170327 xinxinTao

---
+++ commit/20171019/19170327/B1008.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+#define N 100
+
+int main()
+
+{
+
+	    int a[N];
+
+	        int i,j,k,n,m,temp=0;
+
+		    scanf("%d %d",&n,&m);
+
+		        i=0;    
+
+			    while(i<n)
+
+				        {
+
+	   scanf("%d",&a[i]);
+
+	 i++;
+}
+	  for(j=0;j<m;j++) 
+
+					    {
+            temp=a[n-1];
+	for(k=n-1;k>=0;k--)     
+
+									            {       
+
+											 a[k]=a[k-1];
+											 if(k==0)
+											{
+																			                a[k]=temp;
+	
+																				            }
+
+											}
+										        }
+
+	 for(i=0;i<n;i++)
+{
+	 if(i==n-1)								            printf("%d",a[i]);
+	else
+										            printf("%d ",a[i]);
+}
+       return 0;
+}
---
+++ commit/20171019/19170327/B1009.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+	 char a[81];
+
+	  gets(a);
+
+	   int i;
+
+	    for(i=strlen(a)-1;i>=0;i--)
+
+		     {
+
+	      if(a[i]==' ')
+
+				         {
+
+		 printf("%s",&a[i+1]);
+
+	      printf("%c",a[i]);
+
+	         a[i]='\0';
+
+			  }
+
+	         if(i==0)
+
+					   {
+		 printf("%s",&a[i]);
+			 }
+				   }
+	       return 0;
+}
