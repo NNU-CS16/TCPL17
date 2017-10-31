0cffde0d032d1e932e05e5f87efef165dfea1dab
root <2365694766@qq.com>
2017-10-31 23:48:47 +0800

    add work for"20171019-22171913,zhanglv"

---
+++ commit/20171019/22171913/7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    char c;
+    int m,N,i,j;
+    scanf("%d %c",&N,&c);
+    for(m=1;2*m*m-1<=N;m++)
+	    m--;
+       for(i=0;i<2*m-1;i++)
+       {
+	  for(j=0;j<m-1-fabs(m-1-i);j++)       //打印空格
+		  putchar(' ');     
+	  for(j=0;j<2*fabs(m-1-i)+1;j++)
+	          putchar(c);	  
+          putchar('\n');
+       }
+       printf("%d",N-2*m*m+1);
+       return 0;
+}
