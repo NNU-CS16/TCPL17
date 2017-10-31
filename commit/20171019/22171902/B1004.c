4cc3c7321db75b196ac2bb5b6ae46f7a156294f3
root <422329680@qq.com>
2017-10-31 23:58:34 +0800

    Add work for 22171019-22171902,Fanghanyu

---
+++ commit/20171019/22171902/B1004.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+struct student
+{
+		char name[15];
+		char stuid[15];
+		int score;
+}stuinfo,max,min;
+
+int main()
+{
+	int i,n;
+	max.score=0;
+	min.score=100;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%s%s%d",&stuinfo.name,&stunifo.stuid,&stuinfo.score);
+		if(stuinfo.score<=min.score)
+			min=stuinfo;
+		if(stuinfo.score>=max.score)
+			max=stuinfo;
+	}
+	printf("%s %s\n",max.name,max.stuid);
+	printf("%s %s",min.name,min.stuid);
+
+	return 0;
+
+}
+
+
