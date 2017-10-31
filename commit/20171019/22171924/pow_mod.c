d3bff7195d334cc983d800c4dbf2db2ad57a919e
Moran <nsdjsmr@163.com>
2017-10-31 22:28:50 +0800

    Add work for 20171019-22171924.Moran

---
+++ commit/20171019/22171924/pow_mod.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,m,n;
+    scanf("%d%d%d",&a,&b,&c);
+    m=pow(a,b);
+    n=m%c;
+    printf("n=%d\n",n);
+
+    return 0;
+}
+   
+   
