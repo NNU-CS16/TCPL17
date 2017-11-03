26cc600192fd1d0d035bb3f8dd7cba5ffeb15ce8
19170318 <634202016@qq.com>
2017-11-03 09:57:47 +0800

    Add work for 20171102 - 19170318,Yimin Chen

---
+++ commit/20171102/19170318/3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int is_pow2(int n)
+{
+    int t=2;
+    while (t<n) t*=2;
+    if (t==n) return 0;
+    else return -1;
+}
+
+int main()
+{
+    
+    int n;
+    scanf("%d",&n);
+    
+    printf("%d\n",is_pow2(n));
+
+    return 0;
+
+}
---
+++ commit/20171102/19170318/8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int t;
+
+int LCM(int m,int n)
+{
+    int z=m%n;
+    while (z)
+    {
+      m=n;
+      n=z;
+      z=m%n;
+    }
+    return t/n;
+}
+
+int main()
+{
+   
+    int m,n;
+    scanf("%d%d",&m,&n);
+    t=m*n;
+    if (n>m) { m=m^n; n=m^n; m=m^n; }
+    printf("%d\n",LCM(m,n));
+    return 0;   
+
+}
