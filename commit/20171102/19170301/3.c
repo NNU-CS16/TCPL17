1536153d73f982bec9b4d37c68f401c0fef350a3
“Yuhan <1013829049@qq.com>
2017-11-08 00:39:46 +0800

    Add work for 20171102-19170301 Yuhan Wang

---
+++ commit/20171102/19170301/3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int is_pow2(int n)
+{
+  if((n & (n-1)) == 0)
+  
+ return 0;
+  else 
+   
+   return -1;
+}
+int main()
+{
+  int n;
+  scanf("%d",&n);
+ if(is_pow2(n)==0)
+  printf("yes\n");
+  else
+   printf("no\n");
+  return 0;
+}
+  
