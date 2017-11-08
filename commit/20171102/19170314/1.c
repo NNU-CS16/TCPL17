3f799e34412683310a079404498808c83022b578
Lijun Wang <827006477@qq.com>
2017-11-07 21:51:07 -0800

    add work for 20171102 -- 19170314 Lijun Wang

---
+++ commit/20171102/19170314/1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void triangle_judge(int a,int b,int c)
+{
+    
+    
+    if(a+b<c||a+c<b||b+c<a){
+
+    printf("not a triangle");}
+
+    else if(a==b&&b==c){
+    printf("it is a regular triangle");}
+    else if(a==b||a==c||b==c){
+    printf("it is a isosceles triangle");}
+    else {
+    printf("it is a formal triangle");
+    }
+}
+int main()
+{
+    int i;int j;int k;
+    scanf("%d%d%d",&i,&j,&k);
+    triangle_judge(i,j,k);
+    return 0;
+} 
+
