96c40ff52cd3b30a12f4d2c7d59912228b7d407b
“Yuhan <1013829049@qq.com>
2017-11-08 23:16:51 +0800

    Add for work 20171102- 19170301 Yuhan Wang

--- commit/20171102/19170301/10.c
+++ commit/20171102/19170301/10.c
@@ -1,29 +1,26 @@
-#include<stdio.h>
-#include<string.h>
-int is_str_pal(const char* str)
-{
-  int t = strlen(str);
-  int i;
-  for(i = 0;i <= t;i++,t--)
-{
-     if(str[i] != str[t-1])
-    return -1;
-    
-}
+ #include <stdio.h>
+ #include <string.h>
+ int is_str_pal (const char* str)
+ {
+    int t = strlen (str);
+    int i;
+    for (i = 0; i <= t; i++, t--)
+    {
+        if (str[i] != str[t-1])
+            return -1;
+    }
+        return 0;
+ }
+ int main ()
+ {
+    char str[100];
+    scanf ("%s", str);
+    if (is_str_pal (str) == 0)
+        printf ("是回文串");
+    else
+        printf ("不是回文串");
     return 0;
-}
-int main()
-{
-
- char str[100];
-scanf("%s",str);
- 
-if(is_str_pal(str) == 0)
-printf("是回文串");
-else
-printf("不是回文串");
-return 0;
-}
+ }
 
   
 
--- commit/20171102/19170301/11.c
+++ commit/20171102/19170301/11.c
@@ -1,18 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-void reverse(char* str)
-{
- int i=0,t=strlen(str);
-    for(i = t - 1;i >= 0;i--)
-    {
-        printf("%c",str[i]);
-   }
-}
-int main()
-{
-char str[100];
-scanf("%s",str);
-reverse(str);
-printf("\n");
-return 0;
-} 
+ #include <stdio.h>
+ #include <string.h>
+ void reverse (char* str)
+ {
+    int i = 0, t = strlen (str);
+    for (i = t - 1; i >= 0; i--)
+        printf ("%c", str[i]);
+ }
+ int main ()
+ {
+    char str[100];
+    scanf ("%s", str);
+    reverse (str);
+    printf ("\n");
+    return 0;
+ } 
