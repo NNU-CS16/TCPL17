7a602c59795b8855a978b73b683157cbd10dd600
19170318 <634202016@qq.com>
2017-11-08 22:11:48 +0800

    Add work for 20171102 - 19170318, Yimin Chen

--- commit/20171102/19170318/4.c
+++ commit/20171102/19170318/4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int count1_in_bin(int n)
+int count1_in_bin( int n )
 {
-    if (n==0) return 0;
-    else return count1_in_bin(n/2)+n%2;
+    if ( n == 0 ) return 0;
+    else return count1_in_bin( n / 2 ) + n % 2;
 }
 
 int main()
 {
    
     int n;
-    scanf("%d",&n);
-    printf("%d\n",count1_in_bin(n));
+    scanf( "%d", &n );
+    printf( "%d\n", count1_in_bin( n ) );
     return 0;
 
 }
