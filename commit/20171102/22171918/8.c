0d4f64f96d75fc1eb3dcb9efbcb385e22f938218
Zhou Yang <1802870617@qq.com>
2017-11-10 21:13:18 +0800

    Add work for 20171102 - 22171918,Zhou Yang

--- commit/20171102/22171918/8.c
+++ commit/20171102/22171918/8.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
-int LCM(int m,int n);
+int LCM(int m, int n);
 int main ()
 {
-   int m,n;
-   scanf("%d%d",&m,&n);
-   printf("%d\n",LCM(m,n));
-   return 0;
+	int m, n;
+    scanf("%d%d", &m, &n);
+    printf("%d\n", LCM(m, n));
+  return 0;
 }
-int LCM(int m,int n)
+int LCM(int m, int n)
 {
-  int i,j;
-  i=m*n;
-  if(n>m)
-  {
-   m= m^ n;
-   n= n^ m;
-   m= m^ n;
-  }
-  j=m%n;
-  while(j)
-  {
-    m=n;
-    n=j;
-    j=m%n;
-  }
+	int i, j;
+    i = m*n;
+    if (n > m)
+    { 
+		m= m^ n;
+        n= n^ m;
+        m= m^ n;
+    }
+    j = m%n;
+    while (j)
+    {
+    	m = n;
+    	n = j;
+        j = m%n;
+    }
   return i/n;
 }
--- commit/20171102/22171918/9.c
+++ commit/20171102/22171918/9.c
@@ -2,21 +2,21 @@
 int is_int_pal(int n);
 int main ()
 {
-   int n;
-   scanf("%d",&n);
-   printf("%d\n",is_int_pal(n));
-   return 0;
+	int n;
+    scanf("%d", &n);
+    printf("%d\n", is_int_pal(n));
+  return 0;
 }
 int is_int_pal(int n)
 {
-   int i=0,j=n;
-   while(j)
-   {
-     i=i*10+j%10;
-     j/=10;
-   }
-   if(i==n)
-   return 0;
-   else
-   return -1;
+	int i = 0, j = n;
+    while (j)
+    {
+		i = i*10+j%10;
+        j/=10;
+    }
+    if (i == n)
+    return 0;
+    else
+    return -1;
 }
