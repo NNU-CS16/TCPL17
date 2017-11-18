447ecb28d86a800fd02430fa8e7702874006e6ba
chunyu feng <1061579279@qq.com>
2017-11-18 18:24:50 +0800

    Add work for 20171109-22171907fengchunyu

---
+++ commit/20171109/22171907/B1057.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a[]= { 0, 0 };
+    long int n = 0;
+    char c = getchar();
+    while ( c != '\n' )
+    {
+        if ( c >= 'A' && c <= 'Z' ) n += c - 64;
+        if ( c >= 'a' && c <= 'z' ) n += c - 96;
+        c = getchar();
+    }
+
+    while ( n > 0 )
+    {
+        a[n % 2]++;
+        n >>= 1;
+    }
+
+    printf( "%d %d\n", a[0], a[1] );
+
+    return 0;
+
+}
