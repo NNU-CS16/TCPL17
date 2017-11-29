3a5fba727c1bf065a2381e31bf399c51b8039eca
root <422329680@qq.com>
2017-11-30 00:16:24 +0800

    Add work for 20171116-22171902,Fanghanyu

---
+++ commit/20171116/22171902/15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[])
+{
+    FILE* p;
+    char a;
+    if ((p = fopen(* (argv + 1), "r")) == NULL)
+    {
+        printf("无法打开文件!\n");
+        exit(1);
+    }
+    a = fgetc(p);
+    while (feof(p) == 0)
+    {
+        putchar(a);
+        a = fgetc(p);
+    }
+    printf("\n");
+    if (fclose(p))
+        printf("文件无法关闭!\n");
+     return 0;
+}
+
