bdb6d63ad3c04be221445369689f496491ce36c4
Yanjing Jiang <2248896641@qq.com>
2017-12-02 22:28:22 +0800

    Add work for 20171123-22171921,Yanjing Jiang

--- commit/20171123/3.c
+++ commit/20171123/3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 int sum (int n);
 int main()
 {
     int n,p;
     scanf("%d",&n);
     p = sum(n);
-    printf("%d",p);
+    printf("%d\n",p);
     return 0;
 }
 int sum (int n)
