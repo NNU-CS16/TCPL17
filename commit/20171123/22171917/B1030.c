908cf3155b8978a396af47eb7a7fdcd490a68abe
Luoxiaoyu <1192548434@qq.com>
2017-12-06 13:34:54 +0800

    Add work for 20171123 - 22171917,luoxiaoyu

--- commit/20171123/22171917/B1030.c
+++ commit/20171123/22171917/B1030.c
@@ -1,2 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cmp(const void *a,const void *b);
+
+
+int main()
+{
+   int n,i,j,b,sort,s=0;
+   double k,z,arr[10010];
+   scanf("%d%f",&n,&k);
+   for(i=0;i<n;i++)
+   {
+     scanf("%f",&arr[i]);
+   }
+   qsort(b,n,sizeof(double),sort);
+   for(i=0;i<n;i++)
+   {
+   for(j=n-1;j>0;j--)
+   {
+   if(arr[j]<=arr[i]*k)
+    {
+     if(s<j-i)
+     {
+       s=j-i;
+     }
+     else break;
+    }
+   }
+   }
+   printf("%d\n",s+1);
+   return 0;
+}
+
+
+int cmp(const void *a,const void *b)
+{
+    return *((double *)a)-*((double *)b);
+}
 
 
