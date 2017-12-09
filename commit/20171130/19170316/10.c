54eab6fc32f364f251fbcf29a6a042431449d5d6
19170316 <892600409@qq.com>
2017-12-09 23:22:01 +0800

    add homework for 20171130 19170316zhangliang

---
+++ commit/20171130/19170316/10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,j,num=0;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++)
+	scanf("%d",arr+i);
+for(i=0;i<=n-2;i++)
+	for(j=i+1;j<n;j++)
+		if(arr[i]>arr[j])
+			num++;
+	printf("%d\n",num);
+return 0;
+}
+
