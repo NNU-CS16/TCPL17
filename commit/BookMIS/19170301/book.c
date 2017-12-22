30f37f450c9147e1002c5a51e9d4772047cb950b
“Yuhan <1013829049@qq.com>
2017-12-22 21:15:18 +0800

    Add work for BookMIS - 19170301 Yuhan Wang

---
+++ commit/BookMIS/19170301/book.c
@@ -0,0 +1,24 @@
+ #include"hehe.h"
+ int main ()
+ {
+    int i;
+    book head;
+    book* p;
+    p = &head;
+    FILE* fp;
+    if ((fp = fopen ("book.csv", "w")) == NULL)
+    {
+        printf ("无法打开文件\n");
+        exit (1);
+    } 
+    for (i = 0; i < 15; i++)
+    {
+        p = (book*)malloc(sizeof(book));        
+        scanf ("%s %s %d %lf",p -> name, p -> writer, &p -> ISBN, &p -> price);
+        fprintf (fp,"\t\t%s\t\t\t%s\t\t\t%d\t\t\t%lf\n", p -> name, p -> writer, p -> ISBN, p -> price);
+        p -> next = NULL;
+        p = p -> next;
+    }
+    fclose (fp);
+    return 0;
+ }
