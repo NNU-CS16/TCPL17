807198d5e732c1af72b4a08970c23b54385d10de
Yanjing Jiang <2248896641@qq.com>
2017-12-30 23:54:05 +0800

    Add work for BookMIS-22171921,Yanjing Jiang

--- commit/BookMIS/22171921/delete.c
+++ commit/BookMIS/22171921/delete.c
@@ -8,32 +8,32 @@ void delete()
     scanf("%d", &i);
     while(i)
     {
-	char arr[50];
+		char arr[50];
         book* p = head;
-	book* q;
-	printf("\t\t删除的书名：");
-	scanf("%s", arr);
-	while(strcmp(arr, p -> book_name))
-	{
-	    q = p;
-	    p = p -> next;
-	}
-	q -> next = p -> next;
-	free(p);
-	printf("\t\t1.继续删除\n");
-	printf("\t\t0.退出\n");
-	scanf("%d", &i)
-    }
+		book* q;
+		printf("\t\t删除的书名：");
+		scanf("%s", arr);
+		while(strcmp(arr, p -> book_name))
+		{
+		    q = p;
+		    p = p -> next;
+		}
+		q -> next = p -> next;
+		free(p);
+		printf("\t\t1.继续删除\n");
+		printf("\t\t0.退出\n");
+		scanf("%d", &i);
+   	}
     FILE* fp;
-    if ((fp = fopen("book.csv", "w")) = NULL)
+    if ((fp = fopen("book.csv", "w")) == NULL)
     {
-	printf("无法打开文件!")
-	exit(1);
+		printf("无法打开文件!");
+		exit(1);
     }
     while(head != NULL)
     {
-	fprintf(fp, "\t\t%s\n\t\t%s\n\t\t%s\n\t\t%lf\n", head -> book_name, head -> author, head -> ISBN, head -> price);
-	head = head -> next;
+		fprintf(fp, "\t\t%s\n\t\t%s\n\t\t%s\n\t\t%lf\n", head -> book_name, head -> author, head -> ISBN, head -> price);
+		head = head -> next;
     }
     fclose(fp);
 }
--- commit/BookMIS/22171921/read.c
+++ commit/BookMIS/22171921/read.c
@@ -1,30 +1,30 @@
 #include"head.h"
-book* read();
+book* read()
 {
     book* head = (book*)malloc(sizeof(book));
     char a[50],b[20], c[8];
     double j;
     book* p = head;
     book* k = head;
-    P -> next = NULL;
-    FILE* fp = fopen("book.csv", "r");
+    p -> next = NULL;
+    FILE* r = fopen("book.csv", "r");
     if (r == NULL)
     {
-	printf("无法打开文件！");
-	exit(1);
+		printf("无法打开文件！");
+		exit(1);
     }
-    while((fp, %s%s%s%lf, a, b, c, &j) != EOF);
+    while(fscanf (r, "%s %s %s %lf", a, b, c, &j) != EOF);
     {
-	k = (book*)malloc(sizeof(book));
-	strcpy(k -> book_name, a);
-	strcpy(k -> auhthor, b);
-	strcpy(k -> ISBN, c);
-	k -> price = j;
-	p -> next = k;
-	p = k;
+		k = (book*)malloc(sizeof(book));
+		strcpy(k -> book_name, a);
+		strcpy(k -> author, b);
+		strcpy(k -> ISBN, c);
+		k -> price = j;
+		p -> next = k;
+		p = k;
     }
-    P -> next = NULL;
-    fcolse(fp);
-    Return head -> next;
+    p -> next = NULL;
+    fclose (r);
+    return head -> next;
 }
 
