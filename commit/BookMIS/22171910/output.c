5e7e9de1d20ab80af2e5dada64a20de64922087d
22171910吴芳媛 <1216172122@qq.com>
2017-12-30 21:00:35 +0800

    Add work for BookMIS-22171910 Fangyuan Wu

--- commit/BookMIS/22171910/output.c
+++ commit/BookMIS/22171910/output.c
@@ -4,41 +4,12 @@
 #include "head.h"
 void output(struct Node *head)
 {
-  struct Node *p, *temp;
-  FILE *fp;
-  fp = fopen("bookinfo.csv", "r");
-  if (fp == NULL)
+  struct Node *p;
+  p = head;
+  if (p != NULL)
   {
-    printf("Cannot open his file!\n");
-    exit(0);
-  }
-  head = (struct Node *)malloc(sizeof(struct Node));
-  if(!head)
-  {
-    printf("\n application failed");
-    return 0;
-  }
-  head -> next = NULL;
-  temp = head;
-  while (fgetc(fp) != EOF)
-  {
-    fseek (fp, -1, 1);
-    p = (struct Node*)malloc(sizeof(struct Node));
-    if (!p)
-    {
-      printf("\n application failed");
-      return 0;
-    }
-    fscanf(fp, "%s", p -> data.name);
-    fscanf(fp, "%s", p -> data.writer);
-    fscanf(fp, "%s", p -> data.ISBN);
-    fscanf(fp, "%lf", &p -> data.price);
     printf("%s  %s  %s  %lf\n", p -> data.name, p -> data.writer,
                                 p -> data.ISBN, p -> data.price);
-    p -> next = NULL;
-    temp -> next = p;
-    temp = p;
+    p = p -> next;
   }
-  fclose(fp);
-  return 0;
 }
