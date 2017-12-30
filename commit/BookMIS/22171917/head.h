aef91bd20670e2791c1b22e1b569f0a5589843b7
Luoxiaoyu <1192548434@qq.com>
2017-12-30 19:27:30 +0800

    Add work for BookMIS - 22171917,luoxiaoyu

---
+++ commit/BookMIS/22171917/head.h
@@ -0,0 +1,18 @@
+#ifndef HEAD_H
+#define HEAD_H
+struct book
+{
+    char name[50];  char author[20];
+    char ISBN[8];  double price;
+    int num;
+    struct book *next;
+};
+struct book *search(struct book *head);
+struct book *add(struct book *head); 
+struct book *change(struct book *head);
+struct book *delete(struct book *head);
+struct book *account(struct book *head);
+void display(struct book *head);  
+void InFoSta(struct book *head);
+void file(struct book *head);
+#endif
