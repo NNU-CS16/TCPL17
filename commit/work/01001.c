55832eb5a82bf660aa817d7ad282bebab6085d5a
Yuntao <hyt234138@126.com>
2018-02-06 02:00:30 +0800

    Add work for ff

--- commit/work/01001.c
+++ commit/work/01001.c
@@ -2,19 +2,17 @@
 
 int main()
 {
-    int n ,i=0;
+    int n ,i = 0;
     scanf("%d",&n);
-    while(n!=1)
+    while( n )
     {
-         if(n%2==0)
-           {
-                n=n/2;
-           }
+        if(n%2==0)
+            n=n/2;
         else
-          {
-                n=(3*n+1)/2;
-           }
-    i++;
+            n=(3*n+1)/2;
+        i++;
+	if ( n==1 )
+	    break;
     }
     printf("%d\n",i);
     return 0;
--- commit/work/1001.c
+++ commit/work/1001.c
@@ -4,17 +4,13 @@ int main()
 {
     int n ,i;
     scanf("%d",&n);
-    for(i=0;n!=1;i++)
+    for ( i = 0; n!=1; i++ )
     {
-         if(n%2==0)
-           {
-                n=n/2;
-           }
+        if(n%2==0)
+            n=n/2;
         else
-          {
-                n=(3*n+1)/2;
-           }
+            n=(3*n+1)/2;
     }
-    printf("%d",i);
+    printf("%d\n",i);
     return 0;
 }
